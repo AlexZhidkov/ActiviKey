92816c58fc362eafca4125e70d4340e33821d2d0
Alexander Zhidkov <azhidkov@gmail.com>
2020-07-18 17:36:06 +0800

    events on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Event } from '../model/event';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isLoading = true;
+  private eventsCollection: AngularFirestoreCollection<Event>;
+  events: Observable<Event[]>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.eventsCollection = this.afs.collection<Event>('events');
+    this.events = this.eventsCollection.valueChanges({ idField: 'id' });
+    this.isLoading = false;
   }
 
 }
